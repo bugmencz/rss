827b5ba863ad6382bb6d0bc7ae30a460f87d0510
bugmencz <bugmen@centrum.cz>
2025-12-18 08:51:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const sourcesByRegion = {
     { name: "Fox", url: "https://moxie.foxnews.com/google-publisher/us.xml", type: "xml" },
     { name: "NBC", url: "https://feeds.nbcnews.com/feeds/topstories", type: "xml" },
     { name: "NPR", url: "https://feeds.npr.org/1001/rss.xml", type: "xml" },
-    { name: "Politico", url: "https://www.politico.com/rss/politicopicks.xml", type: "xml" },
+    { name: "Politico", url: "https://rss.politico.com/politics-news.xml", type: "xml" },
     { name: "Real Clear Politics", url: "https://www.realclearpolitics.com/index.xml", type: "xml" },
     { name: "The New York Times", url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml", type: "xml" },
   ],
@@ -293,6 +293,7 @@ function parseRSSXML(xmlString) {
 
   return articles;
 }
+
 
 
 
